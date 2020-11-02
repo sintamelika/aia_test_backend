8ce6876693cd0810c5f260e6694ecee64c6831d0
sinta amelika <sinta.amelika@gmail.com>
2020-11-02 23:31:42 +0700

    - add function get public photo - update api get photos_public

--- index.js
+++ index.js
@@ -3,15 +3,29 @@
 const restify = require('restify');
 const https = require('https');
 
-function respond(req, res, next) {
-	res.send('test');
-	next();
+function getPublicPhoto(req, res, next) {
+	https.get('https://www.flickr.com/services/feeds/photos_public.gne', (resp) => {
+	  let data = '';
+
+	  // A chunk of data has been received.
+	  resp.on('data', (chunk) => {
+	    data += chunk;
+	  });
+
+	  // The whole response has been received. Print out the result.
+	  resp.on('end', () => {
+	    res.send(data);
+  		return;
+	  });
+
+	}).on("error", (err) => {
+	  console.log("Error: " + err.message);
+	});
   
 }
 
 var server = restify.createServer();
-
-server.get('/photos_public', respond);
+server.get('/photos_public', getPublicPhoto);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
